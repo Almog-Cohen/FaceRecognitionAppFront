1ec294f3f1301d49b2b2d378b07d3e8033849524
Almog Cohen <almogco94@gmail.com>
2020-12-05 20:17:17 +0200

    Connect front end to heroku

--- src/components/Constans/Fetch.js
+++ src/components/Constans/Fetch.js
@@ -1,10 +1,10 @@
 // Fetch urls
-export const SIGN_IN_URL = 'http://localhost:3001/signin';
-export const REGISTER_URL = "http://localhost:3001/register";
-export const IMAGE_URL = 'http://localhost:3001/imageurl';
-export const PROFILE_URL = 'http://localhost:3001/profile/';
-export const IMAGE_UPDATE = 'http://localhost:3001/image';
-export const SIGN_OUT_URL = "http://localhost:3001/signout";
-export const REMOVE_USER_URL = "http://localhost:3001/removeUser/";
-export const RANK_LIST_URL = "http://localhost:3001/rank-list";
+export const SIGN_IN_URL = ' https://glacial-beach-93669.herokuapp.com/signin';
+export const REGISTER_URL = " https://glacial-beach-93669.herokuapp.com/register";
+export const IMAGE_URL = ' https://glacial-beach-93669.herokuapp.com/imageurl';
+export const PROFILE_URL = ' https://glacial-beach-93669.herokuapp.com/profile/';
+export const IMAGE_UPDATE = ' https://glacial-beach-93669.herokuapp.com/image';
+export const SIGN_OUT_URL = " https://glacial-beach-93669.herokuapp.com/signout";
+export const REMOVE_USER_URL = " https://glacial-beach-93669.herokuapp.com/removeUser/";
+export const RANK_LIST_URL = " https://glacial-beach-93669.herokuapp.com/rank-list";
 export const RANK_LAMBDA_URL = 'https://1hss8cyv6e.execute-api.us-east-1.amazonaws.com/prod/rank?rank=';
